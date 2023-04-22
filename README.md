cb655ed3b2e01095c8696f7d8f23c8f20fbe2ef2
root <franklinchilaka94@gmail.com>
2023-04-22 16:06:17 -0700

    C-random-code-generator-

---
+++ README.md
@@ -0,0 +1,1 @@
+ A simple C program that prints a random number
---
+++ randomnumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// A simple C program that prints a random number//
+// Written by Frankdegret//
+
+int main() {
+
+   int length = 4; // length of the random word
+   int i;
+
+   srand(time(NULL)); // seed the random number generator
+
+   printf("Random number: ");
+   for (i = 0; i < length; i++) {
+      int numbers = rand() %10 ; // generate a random number between 0 and 10
+      printf("%d", numbers);
+   }
+   printf("\n");
+
+   return 0;
+} 
+
